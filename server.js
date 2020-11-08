7a4031b7a82bbbee579accbdf500dd3146c3174d
Jack Andrews <japotts16@gmail.com>
2020-11-08 19:06:13 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+const express = require('express'); 
+const path = require('path');
+const app = express();
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const colors = require('colors');
+
+const connectDB = require('./config/DB');
+dotenv.config({ path: './config/config.env'});
+
+connectDB()
+
+// DB models
+
+// Routes
+
+// Static Files
+
+// Express server for api and libraries
+app.use(cors())
+app.use(express.json())
+
+// Morgan API Logger
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+
+// Express web server PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+    )
+)
