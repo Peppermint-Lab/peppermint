c1bf930824e7c94f3c74ecaba40e8f17e1ca8f2b
Jack Andrews <japotts16@gmail.com>
2020-11-08 23:32:22 +0000

    auth middleware

---
+++ middleware/authCheck.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config/keys')
+const mongoose = require('mongoose')
+const User = mongoose.model("InternalUser");
+
+module.exports = (req, res, next) => {
+    const {authorization} = req.headers
+    if (!authorization) {
+        return res.status(401).json({error: "You must be logged in"})
+    }
+    const token = authorization.replace("Bearer ", "")
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+        if(err){
+         return  res.status(401).json({error:"you must be logged in"})
+        }
+
+        const {_id} = payload
+        User.findById(_id).then(userdata=>{
+            req.user = userdata
+            next()
+        })
+        
+    })
+}
