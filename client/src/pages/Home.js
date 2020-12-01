082d50c81414ec40cfdfa57992b47f5db5c4b1f8
Jack Andrews <japotts16@gmail.com>
2020-12-01 19:41:51 +0000

    redo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { Container } from "rsuite";
+import { Container, Header } from "rsuite";
 
-
-
-
-
-
+import Navigation from "../component/Navigation";
 
 
 const Home = () => {
   return (
     <div>
       <Container>
-        
+        <Header>
+          <Navigation />
+        </Header>
       </Container>
     </div>
   );
