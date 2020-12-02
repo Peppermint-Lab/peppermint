e0767afc17dc199f6acaa0148596facd87d1c2c9
Jack Andrews <jack@jandrews.dev>
2020-12-02 23:45:02 +0000

    info

--- client/src/component/TicketInfo.js
+++ client/src/component/TicketInfo.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+
 const TicketInfo = (props) => {
+
+    console.log(props)
+
+    const data = props 
     
     return (
         <div>
             
+            <div className="top-left">
+            {data.issue}
+            </div>
+
+            <div className="top-right">
+
+            </div>
+
+            <div className="bottom-right">
+
+            </div>
+
+            <div className="bottom-left">
+
+            </div>
+
         </div>
     )
 }
