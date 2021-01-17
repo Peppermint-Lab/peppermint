d716b4bb62c6e26981ef801ae618bb96117fd248
Jack Andrews <jack@jandrews.dev>
2021-01-17 03:10:31 +0000

    nav update for user

--- client/src/component/Navigation.js
+++ client/src/component/Navigation.js
@@ -99,42 +99,45 @@ const Navigation = () => {
     } else {
       return (
         <div>
-          <Menu
-            mode="horizontal"
-            onClick={handleClick}
-            selectedKeys={[current]}
-          >
-            <Menu.Item disabled={true}>Project Winter</Menu.Item>
-            <Menu.Item key={0} onClick={() => history.push("/")}>
-              Home
-            </Menu.Item>
-            <SubMenu key="SubMenu" icon={<FileTwoTone />} title="Tickets">
+          <Layout>
+            <Menu
+              mode="horizontal"
+              onClick={handleClick}
+              defaultSelectedKeys={["0"]}
+              selectedKeys={current}
+            >
+              <Menu.Item key="Home Page">
+                <a
+                  href="https://pmint.dev/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Peppermint 🍵
+                </a>
+              </Menu.Item>
+              <Menu.Item key={0} onClick={() => history.push("/")}>
+                Home
+              </Menu.Item>
+              <SubMenu key="SubMenu" icon={<FileTwoTone />} title="Tickets">
                   <Menu.Item key="tickets:1"><Link to='/ticket/open'>Open Tickets</Link></Menu.Item>
                   <Menu.Item key="tickets:2"><Link to='/ticket/unissued'>Unissued Tickets</Link></Menu.Item>
               </SubMenu>
-            <Menu.Item key={3} onClick={() => history.push("/history")}>
-              History
-            </Menu.Item>
-            <Menu.Item
-              key={5}
-              style={{ float: "right" }}
-            >
-              Settings
-            </Menu.Item>
-            <Menu.Item
-              key={4}
-              style={{ float: "right" }}
-            >
-              <NewTicket />
-            </Menu.Item>
-            <Switch
-              style={{ float: "right", marginTop: 15 }}
-              checked={isDark === "dark"}
-              onChange={changeTheme}
-              checkedChildren="Dark"
-              unCheckedChildren="Light"
-            />
-          </Menu>
+              <Menu.Item key={3} onClick={() => history.push("/history")}>
+                History
+              </Menu.Item>
+              <SubMenu
+                key='settings-men'
+                icon={<SettingTwoTone />}
+                style={{ float: "right" }}
+              >
+                <Menu.Item key="SETTINGS:1"><Link to='/settings'>Settings</Link></Menu.Item>
+                <Menu.Item key="SETTINGS:2" onClick={logout}>Log out</Menu.Item>
+              </SubMenu>
+              <Menu.Item key={4} style={{ float: "right" }} title="New Ticket">
+                <NewTicket />
+              </Menu.Item>
+            </Menu>
+          </Layout>
         </div>
       );
     }
