e0f7af23daacff1d8540aadbcad5d65a886021cb
Jack Andrews <japotts16@gmail.com>
2021-01-21 23:55:47 +0000

    states

--- client/src/component/time/NewLog.js
+++ client/src/component/time/NewLog.js
@@ -4,25 +4,45 @@ import moment from "moment";
 
 const NewLog = () => {
   const [date, setDate] = useState();
+  const [start, setStart] = useState();
+  const [end, setEnd] = useState();
+  const [activity, setActivity] = useState("");
 
   const format = "HH:mm";
 
-  function onChange(date, dateString) {
+  function onChangeDate(date, dateString) {
     console.log(date);
     setDate(date._d);
+  }
+
+  function onChangeStart(time) {
+    const t = time;
+    const m = moment(t).format("hh:mm")
+    setStart(m)
+  }
+
+  function onChangeEnd(time) {
+    const t = time;
+    const m = moment(t).format("hh:mm")
+    setEnd(m)
   }
 
   return (
     <div className="newlog">
       <Space>
-        <DatePicker onChange={onChange} />
-        <TimePicker defaultValue={moment()} format={format} />
+        <DatePicker onChange={onChangeDate} />
+        <TimePicker
+          defaultValue={moment}
+          format={format}
+          onChange={onChangeStart}
+        />
         <Input
           placeholder="Work completed on ticket ... "
           className="newlog-input"
           style={{ width: 300 }}
+          onChange={(e) => setActivity(e.target.value)}
         />
-        <TimePicker defaultValue={moment()} format={format} />
+        <TimePicker defaultValue={moment()} format={format} onChange={onChangeEnd} />
         <Button>Submit</Button>
       </Space>
     </div>
