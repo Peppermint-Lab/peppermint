256c52545fed94dfa3f78c321f7c20d5d32dd3ab
Jack Andrews <jack@jandrews.dev>
2021-02-08 00:17:36 +0000

    remove load test

--- k6/loginCheck.js
+++
@@ -1,9 +0,0 @@
-import http from 'k6/http';
-import {check, sleep} from 'k6';
-
-export default function() {
-  const data = {email: 'admin@admin.com', password: '12345'};
-  let res = http.post('http://localhost:3000/login', data);
-    check(res, { 'success login': (r) => r.status === 200 });
-    sleep(0.3);
-}
