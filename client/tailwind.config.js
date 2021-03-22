2a61028507ce2af7358d659e646c263cc6b31db6
Potts99 <japotts16@gmail.com>
2021-03-22 23:41:46 +0000

    craco & tailwind support

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+  // tailwind.config.js
+  module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {},
+     },
+     variants: {
+       extend: {},
+     },
+     plugins: [],
+   }
