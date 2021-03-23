de65d6ab9a6a05f203554d190d1f2e01b67c79a6
Potts99 <japotts16@gmail.com>
2021-03-23 23:23:25 +0000

    up

--- client/src/components/home/Main.js
+++ client/src/components/home/Main.js
@@ -170,55 +170,6 @@ const Main = () => {
                             </p>
                           </div>
                         </li>
-
-                        <li class="py-5">
-                          <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                            <h3 class="text-sm font-semibold text-gray-800">
-                              <Link
-                                href="#"
-                                class="hover:underline focus:outline-none"
-                              >
-                                <span
-                                  class="absolute inset-0"
-                                  aria-hidden="true"
-                                ></span>
-                                New password policy
-                              </Link>
-                            </h3>
-                            <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                              Alias inventore ut autem optio voluptas et
-                              repellendus. Facere totam quaerat quam quo
-                              laudantium cumque eaque excepturi vel. Accusamus
-                              maxime ipsam reprehenderit rerum id repellendus
-                              rerum. Culpa cum vel natus. Est sit autem
-                              mollitia.
-                            </p>
-                          </div>
-                        </li>
-
-                        <li class="py-5">
-                          <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                            <h3 class="text-sm font-semibold text-gray-800">
-                              <Link
-                                href="#"
-                                class="hover:underline focus:outline-none"
-                              >
-                                <span
-                                  class="absolute inset-0"
-                                  aria-hidden="true"
-                                ></span>
-                                Office closed on July 2nd
-                              </Link>
-                            </h3>
-                            <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                              Tenetur libero voluptatem rerum occaecati qui est
-                              molestiae exercitationem. Voluptate quisquam iure
-                              assumenda consequatur ex et recusandae. Alias
-                              consectetur voluptatibus. Accusamus a ab dicta et.
-                              Consequatur quis dignissimos voluptatem nisi.
-                            </p>
-                          </div>
-                        </li>
                       </ul>
                     </div>
                     <div class="mt-6">
@@ -240,109 +191,18 @@ const Main = () => {
                       class="text-base font-medium text-gray-900"
                       id="recent-hires-title"
                     >
-                      Recent Hires
+                      Personal Files
                     </h2>
                     <div class="flow-root mt-6">
                       <ul class="-my-5 divide-y divide-gray-200">
                         <li class="py-4">
                           <div class="flex items-center space-x-4">
-                            <div class="flex-shrink-0">
-                              <img
-                                class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                alt=""
-                              />
-                            </div>
                             <div class="flex-1 min-w-0">
                               <p class="text-sm font-medium text-gray-900 truncate">
                                 Leonard Krasner
                               </p>
                               <p class="text-sm text-gray-500 truncate">
                                 @leonardkrasner
-                              </p>
-                            </div>
-                            <div>
-                              <Link
-                                href="#"
-                                class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                              >
-                                View
-                              </Link>
-                            </div>
-                          </div>
-                        </li>
-
-                        <li class="py-4">
-                          <div class="flex items-center space-x-4">
-                            <div class="flex-shrink-0">
-                              <img
-                                class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                alt=""
-                              />
-                            </div>
-                            <div class="flex-1 min-w-0">
-                              <p class="text-sm font-medium text-gray-900 truncate">
-                                Floyd Miles
-                              </p>
-                              <p class="text-sm text-gray-500 truncate">
-                                @floydmiles
-                              </p>
-                            </div>
-                            <div>
-                              <Link
-                                href="#"
-                                class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                              >
-                                View
-                              </Link>
-                            </div>
-                          </div>
-                        </li>
-
-                        <li class="py-4">
-                          <div class="flex items-center space-x-4">
-                            <div class="flex-shrink-0">
-                              <img
-                                class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                alt=""
-                              />
-                            </div>
-                            <div class="flex-1 min-w-0">
-                              <p class="text-sm font-medium text-gray-900 truncate">
-                                Emily Selman
-                              </p>
-                              <p class="text-sm text-gray-500 truncate">
-                                @emilyselman
-                              </p>
-                            </div>
-                            <div>
-                              <Link
-                                href="#"
-                                class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                              >
-                                View
-                              </Link>
-                            </div>
-                          </div>
-                        </li>
-
-                        <li class="py-4">
-                          <div class="flex items-center space-x-4">
-                            <div class="flex-shrink-0">
-                              <img
-                                class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                alt=""
-                              />
-                            </div>
-                            <div class="flex-1 min-w-0">
-                              <p class="text-sm font-medium text-gray-900 truncate">
-                                Kristin Watson
-                              </p>
-                              <p class="text-sm text-gray-500 truncate">
-                                @kristinwatson
                               </p>
                             </div>
                             <div>
