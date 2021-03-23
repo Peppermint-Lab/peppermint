0f88e78fdd0ebe0f367412e7cef899473f9337e9
Potts99 <japotts16@gmail.com>
2021-03-23 14:04:49 +0000

    login

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'antd/dist/antd.css';
 
 import UserDash from './pages/UserDash'
+import Login from './pages/auth/Login'
 
 const Routing = () => {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={UserDash} />
+
+          <Route exact path="/login">
+            <Login />
+        </Route>
   
           
         </Switch>
--- client/src/pages/UserDash.js
+++ client/src/pages/UserDash.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import Main from "../components/home/Main";
 import Header from '../components/navigation/Header'
 
 const UserDash = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    async function auth() {
+      await fetch(`/api/v1/auth/token`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response)
+          const res = response;
+          if (res.auth === false ) {
+            history.push("/login");
+          } else {
+            return console.log("logged in");
+          }
+        });
+    }
+    auth();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
