6b8e9f060d152052d8e10bb81ac155465cd548a7
Potts99 <japotts16@gmail.com>
2021-04-16 12:12:46 +0100

    seed.js

---
+++ prisma/seed.js
@@ -0,0 +1,28 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@admin.com" },
+    update: {},
+    create: {
+      id: 1,
+      email: `admin@admin.com`,
+      firstName: "admin",
+      LastName: "admin",
+      isAdmin: true,
+      password: "$2b$10$BFmibvOW7FtY0soAAwujoO9y2tIyB7WEJ2HNq9O7zh9aeejMvRsKu",
+    },
+  });
+
+  console.log({ admin });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
