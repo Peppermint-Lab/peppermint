33695a20743fd941bd78d63a6e37fff453ff0793
potts99 <japotts16@gmail.com>
2021-06-08 20:30:13 +0100

    images added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Peppermint</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
