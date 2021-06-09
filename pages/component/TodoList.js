1bad2c1b3943995e3cc6efb9e2167ef130b012a0
Jack Andrews <japotts16@gmail.com>
2021-06-09 15:11:44 +0100

    working fetch

--- pages/component/TodoList.js
+++ pages/component/TodoList.js
@@ -1,9 +1,19 @@
-
+import { useEffect } from "react"
 
 
 export default function TodoList() {
 
-
+    useEffect(() => {
+        fetch(`/api/todos/get`, {
+            method: "get",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).then((res) => res.json())
+            .then((res) => {
+                console.table(res)
+            })
+    })
 
     return (
         <div>
