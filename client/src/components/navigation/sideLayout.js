9dee062e171a1181215bfdd297fb40798bd30449
Jack Andrews <japotts16@gmail.com>
2021-06-13 21:48:29 +0100

    layout issue

--- client/src/components/navigation/sideLayout.js
+++ client/src/components/navigation/sideLayout.js
@@ -209,7 +209,7 @@ const navigation = [
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+          <main className="flex-1 relative z-0 focus:outline-none h-screen">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <div className="py-4">
