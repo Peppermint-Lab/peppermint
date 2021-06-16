c1ce918520142c1707738456f2a96905e2a53767
Jack Andrews <japotts16@gmail.com>
2021-06-16 14:39:54 +0100

    clear cache

--- client/src/hoc/Lazy.js
+++
@@ -1,9 +0,0 @@
-import React, { Suspense } from 'react';
-
-export const withLazyComponent = (LazyComponent) => {
-  return (props) => (
-    <Suspense fallback="Lazy component is loading ...">
-      <LazyComponent {...props} />
-    </Suspense>
-  )
-}
