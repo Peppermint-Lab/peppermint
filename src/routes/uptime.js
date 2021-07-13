d33e23ec439b5df84342d2e63c11038540873312
potts99 <japotts16@gmail.com>
2021-07-13 22:04:34 +0000

    get monitors from db

--- src/routes/uptime.js
+++ src/routes/uptime.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { Add } = require('../controller/uptime/add')
+const { GetAll } = require('../controller/uptime/getAll')
 
 router.route("/add").post(Add);
+router.route("/getAll").get(GetAll);
 
 module.exports = router;
 
