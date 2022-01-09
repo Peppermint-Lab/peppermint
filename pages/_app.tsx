73e519086e6911068d1aac3e6b2e9aa080775738
potts99 <japotts16@gmail.com>
2022-01-09 18:00:28 +0000

    boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useRouter } from "next/router";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+
+  if (router.asPath.slice(0, 5) === "/auth") {
+    return (
+      <>
+        <Component {...pageProps} />
+      </>
+    );
+  }
+
+  return (
+    <>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+    </>
+  );
 }
 
 export default MyApp
