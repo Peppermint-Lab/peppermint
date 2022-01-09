5ecce7f8a859978375c2b1115e5d637be46ce2e1
potts99 <japotts16@gmail.com>
2022-01-09 22:48:40 +0000

    next-auth login route

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,47 +1,48 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { prisma } from "../../../prisma/prisma";
 
 const options = {
-    site: process.env.NEXTAUTH_URL,
-    providers: [
-      Providers({
-        name: "Credentials",
-        async authorize(credentials, req, res) {
-          try {
-            // const { db } = await connectToDatabase();
-            // const user = await db.collection("users").findOne({
-            //   email: credentials.email,
-            // });
-  
-            // if (!user) throw new Error("No user found");
-  
-            // const isPasswordValid = await verifyPassword(
-            //   credentials.password,
-            //   user.password
-            // );
-  
-            // if (!isPasswordValid) throw new Error("Password is not valid");
-  
-            // return {
-            //   email: user.email,
-            //   id: user._id,
-            // };
-          } catch (error) {
-            throw new Error(error);
-          }
-        },
-      }),
-    ],
-    session: {
-      jwt: true,
-      maxAge: 30 * 24 * 60 * 60,
-    },
-    secret: 'lXVsKy1aOGTyAgrqdU5JBa8J5Iu+sRdJ1e9LEH++dtM=',
-    database: process.env.DATABASE_URL,
-    pages: {
-      signIn: "/auth/login",
-    },
-    debug: true,
-  };
-  
-  export default (req, res) => NextAuth(req, res, options);
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+    Providers({
+      name: "Credentials",
+      async authorize(credentials, req, res) {
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: emailLower },
+          });
+
+          if (!user) throw new Error("No user found");
+
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isPasswordValid) throw new Error("Password is not valid");
+
+          return {
+            email: user.email,
+            id: user._id,
+          };
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: "lXVsKy1aOGTyAgrqdU5JBa8J5Iu+sRdJ1e9LEH++dtM=",
+  database: process.env.DATABASE_URL,
+  pages: {
+    signIn: "/auth/login",
+  },
+  debug: true,
+};
+
+export default (req, res) => NextAuth(req, res, options);
