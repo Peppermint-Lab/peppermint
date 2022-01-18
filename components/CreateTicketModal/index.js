21900415740262383fc3a6f4194f7a537fd8d0fc
potts99 <japotts16@gmail.com>
2022-01-18 00:23:08 +0000

    small tweaks

--- components/CreateTicketModal/index.js
+++ components/CreateTicketModal/index.js
@@ -269,7 +269,7 @@ export default function CreateTicketModal() {
                             <div className="mt-1 relative">
                               <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                 <span className="block truncate">
-                                  {engineer ? engineer.name : 'Please select an engineer'}
+                                  {engineer ? engineer.firstName : 'Please select an engineer'}
                                 </span>
                                 <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                   <SelectorIcon
