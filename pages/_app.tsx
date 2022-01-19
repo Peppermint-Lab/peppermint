455cf708e1423a7bc961aa695840c458c515f362
potts99 <japotts16@gmail.com>
2022-01-19 21:16:22 +0000

    auth check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import 'antd/dist/antd.css';
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useRouter } from "next/router";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import SideLayout from '../components/Layout/SideLayout'
 
 const queryClient = new QueryClient();
+
+function Auth({ children }) {
+  const { data: session, status } = useSession({required: true})
+  const isUser = !!session?.user
+
+  console.log(session)
+
+  if (isUser) {
+    return children
+  }
+
+  // Session is being fetched, or no user.
+  // If no user, useEffect() will redirect.
+  return <div>Loading...</div>
+}
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
@@ -25,7 +40,9 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       <SessionProvider session={pageProps.session}>
         <QueryClientProvider client={queryClient}>
         <SideLayout>
+        <Auth>
           <Component {...pageProps} />
+        </Auth>
           </SideLayout>
         </QueryClientProvider>
       </SessionProvider>
