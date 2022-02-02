ad60aed503081909a5bed59a47e8b4dadfcdbebf
Jack Andrews <japotts16@gmail.com>
2022-02-02 12:04:29 +0000

    add head file

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { useRouter } from "next/router";
 import { SessionProvider, useSession } from "next-auth/react";
 import ScaleLoader from "react-spinners/ScaleLoader";
+import Head from 'next/head'
+
 
 import SideLayout from "../components/Layout/SideLayout";
 
@@ -45,6 +47,33 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
   return (
     <>
+     <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Ticket management system selfhosted open source" />
+        <meta name="keywords" content="Keywords" />
+        <title>Peppermint</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <SessionProvider session={session}>
         <QueryClientProvider client={queryClient}>
           <Auth>
