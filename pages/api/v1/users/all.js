9906660391833609ceb2f185b76d517526ccd9ef
Jack Andrews <japotts16@gmail.com>
2022-02-10 00:12:33 +0000

    Ticket transfer now working

--- pages/api/v1/users/all.js
+++ pages/api/v1/users/all.js
@@ -2,7 +2,16 @@ const { prisma } = require("../../../../prisma/prisma");
 
 export default async function getAllClients(req, res) {
   try {
-    const users = await prisma.user.findMany({});
+    const users = await prisma.user.findMany({
+      where: {},
+      select: {
+        email: true,
+        name: true,
+        id: true,
+        isAdmin: true
+      }
+    });
+
     res.json({ users, failed: false });
   } catch (error) {
     console.log(error);
