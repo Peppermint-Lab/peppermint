bb21c058aba1ed7d7d1dea6acc76810d5b321de4
dieideeistgut <me@dieideeistgut.de>
2022-02-16 23:36:03 +0100

    language as a session param. maybe remove this again later.

--- pages/api/v1/users/all.js
+++ pages/api/v1/users/all.js
@@ -8,7 +8,8 @@ export default async function getAllClients(req, res) {
         email: true,
         name: true,
         id: true,
-        isAdmin: true
+        isAdmin: true,
+        language: true
       }
     });
 
