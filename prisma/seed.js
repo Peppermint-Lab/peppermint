0060d4b331a3a313188417380c1b7629c02826c9
dieideeistgut <me@dieideeistgut.de>
2022-02-16 23:38:18 +0100

    migration adding language colum to user table

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,6 +10,7 @@ async function main() {
       name: "admin",
       isAdmin: true,
       password: "$2b$10$BFmibvOW7FtY0soAAwujoO9y2tIyB7WEJ2HNq9O7zh9aeejMvRsKu",
+      language: "en",
     },
   });
 
