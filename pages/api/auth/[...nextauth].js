58eaa5c46b584611d36c713b590300f20b74cc2d
mmoufakkir2 <57078126+mmoufakkir2@users.noreply.github.com>
2023-08-28 21:57:17 -0400

    added new code for auth ms

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { prisma } from "../../../prisma/prisma";
+import AzureADProvider from "next-auth/providers/azure-ad";
 
 const options = {
   site: process.env.NEXTAUTH_URL,
   providers: [
     Providers({
-      name: "Credentials",
-      async authorize(credentials, req, res) {
+      async authorize(credentials) {
         try {
           const user = await prisma.user.findUnique({
             where: { email: credentials.email },
@@ -28,19 +28,23 @@ const options = {
             id: user.id,
             name: user.name,
             isAdmin: user.isAdmin,
-          }
+          };
         } catch (error) {
           throw new Error(error);
         }
       },
     }),
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
   ],
-  secret: "yudjXHbqE5VH4LkwZ4srgsdL2EZrjp",
+  secret: process.env.NEXTAUTH_SECRET, 
   session: {
     jwt: true,
     maxAge: 30 * 24 * 60 * 60,
   },
-
   database: process.env.DATABASE_URL,
   pages: {
     signIn: "/auth/login",
@@ -48,15 +52,23 @@ const options = {
   },
   callbacks: {
     jwt: async ({ token, user }) => {
-      user && (token.user = user);
-
+      if (user) {
+        token.user = user;
+      }
       return token;
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) { 
       session.accessToken = token.accessToken;
       session.id = token.user.id;
-      session.user.isAdmin = token.user.isAdmin;
-      session.user.id = token.user.id;
+      session.user = {
+        ...token.user,
+        id: token.user.id,
+        isAdmin: token.user.isAdmin,
+        // Add additional properties from Azure AD to the session
+        azureAD: {
+          // ... add properties like email, name, etc. from Azure AD
+        },
+      };
       return session;
     },
   },
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,6 +1,6 @@
 import { getCsrfToken } from "next-auth/react";
 import Link from "next/link";
-
+import { signIn } from 'next-auth/react';
 export async function getServerSideProps(context) {
   return {
     props: {
@@ -10,6 +10,10 @@ export async function getServerSideProps(context) {
 }
 
 export default function Login({ csrfToken }) {
+
+  const handleMicrosoftLogin = () => {
+    signIn('azure-ad');
+  };
   console.log(csrfToken);
   return (
     <div>
@@ -116,7 +120,7 @@ export default function Login({ csrfToken }) {
             </form>
           </div>
 
-          {/* <div className="mt-6">
+           <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300" />
@@ -128,7 +132,26 @@ export default function Login({ csrfToken }) {
               </div>
             </div>
 
-            <div className="mt-6 grid grid-cols-3 gap-3">
+            <div className="mt-6">
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-300" />
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 bg-white text-gray-500">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <button
+          onClick={handleMicrosoftLogin}
+          className="mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          Continue with Microsoft
+        </button>
+      </div>
+
+            {/* <div className="mt-6 grid grid-cols-3 gap-3">
               <div>
                 <a
                   href="#"
@@ -183,8 +206,8 @@ export default function Login({ csrfToken }) {
                   </svg>
                 </a>
               </div>
-            </div>
-          </div> */}
+            </div> */}
+          </div> 
         </div>
       </div>
     </div>
