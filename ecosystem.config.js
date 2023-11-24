be5927de25251ee214bb3af5de83a93bd5f89dbb
potts99 <japotts16@gmail.com>
2023-11-24 20:38:34 +0000

    docker container opti

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,6 +29,7 @@ module.exports = {
         DB_USERNAME: process.env.DB_USERNAME,
         DB_PASSWORD: process.env.DB_PASSWORD,
         DB_HOST: process.env.DB_HOST,
+        secret: process.env.SECRET,
       },
     },
   ],
