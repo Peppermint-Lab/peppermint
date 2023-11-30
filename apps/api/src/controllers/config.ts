8ebc5c330f7f2fc8d4097b1cbee0267140f1d760
potts99 <japotts16@gmail.com>
2023-11-30 21:48:12 +0000

    boilerplate for config

---
+++ apps/api/src/controllers/config.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { checkToken } from "../lib/jwt";
+import { prisma } from "../prisma";
+
+// Check Github Version 
+// Add outbound email provider
+// Email Verification
+// SSO Provider
+// Portal Locale
+// Feature Flags
