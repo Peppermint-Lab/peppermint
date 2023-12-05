bbb3f882c42751d6bb0425a427cbfb784b37712a
Jack Andrews <45759583+potts99@users.noreply.github.com>
2023-12-05 21:56:29 +0000

    live site (#204)
    * site
    
    * Create site

--- apps/client/pages/admin/email.tsx
+++ apps/client/pages/admin/email.tsx
@@ -264,7 +264,6 @@ export default function Notifications() {
                         enabled ? "bg-blue-600" : "bg-gray-200"
                       } relative inline-flex h-6 w-11 items-center rounded-full`}
                     >
-                      <span className="sr-only">Enable notifications</span>
                       <span
                         className={`${
                           enabled ? "translate-x-6" : "translate-x-1"
