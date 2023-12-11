25d133483c3190172e6a96733fe42ebc344d5979
DeDuplicate <31889523+DeDuplicate@users.noreply.github.com>
2023-12-11 10:33:06 +0200

    Update index.js

--- apps/client/components/UserProfile/index.js
+++ apps/client/components/UserProfile/index.js
@@ -114,26 +114,25 @@ export function UserProfile() {
                     <option value="en">EN</option>
                     <option value="se">SE</option>
                     <option value="he">HE</option>
-                  </select>
-                </div>
+                </select>
               </div>
             </div>
           </div>
-
         </div>
+      </div>
 
-        <div className="mt-4 py-4 px-4 flex justify-end sm:px-6">
-          <button
-            onClick={async() => {
-              await updateProfile();
-              router.reload();
-            }}
-            type="submit"
-            className="ml-5 bg-light-blue-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-black hover:bg-light-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500"
-          >
-            Save & Reload
-          </button>
-        </div>
-      </>
-    )
+      <div className="mt-4 py-4 px-4 flex justify-end sm:px-6">
+        <button
+          onClick={async () => {
+            await updateProfile();
+            router.reload();
+          }}
+          type="submit"
+          className="ml-5 bg-light-blue-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-black hover:bg-light-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500"
+        >
+          Save & Reload
+        </button>
+      </div>
+    </>
+  );
 }
