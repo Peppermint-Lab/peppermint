248da1fb7ac3c8a52b26b65d8038df53dc608f72
yossef-bismart <83905330+yossef-bismart@users.noreply.github.com>
2023-12-26 20:59:08 +0200

    fix index.js

--- apps/client/components/UserProfile/index.js
+++ apps/client/components/UserProfile/index.js
@@ -111,48 +111,6 @@ export function UserProfile() {
           </div>
         </div>
       </div>
-            <div>
-              <label
-                for="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1 rounded-md shadow-sm flex">
-                <input
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  className="focus:ring-light-blue-500 focus:border-light-blue-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
-                  defaultValue={session.user.email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                for="language"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Language
-              </label>
-              <div className="mt-1 rounded-md shadow-sm flex">
-                <select
-                  id="language"
-                  name="language"
-                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                  defaultValue={router.locale}
-                  onChange={(e) => changeLanguage(e.target.value)}
-                >
-                  <option value="de">DE</option>
-                  <option value="en">EN</option>
-                  <option value="se">SE</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div className="mt-4 py-4 px-4 flex justify-end sm:px-6">
         <button
