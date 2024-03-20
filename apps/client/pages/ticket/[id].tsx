b96f76bd31a31d152a4a2cf4b469bd0fc1232d9e
potts99 <japotts16@gmail.com>
2024-03-20 23:00:35 +0000

    fix explot

--- apps/client/pages/ticket/[id].tsx
+++ apps/client/pages/ticket/[id].tsx
@@ -373,6 +373,7 @@ export default function Ticket() {
                       >
                         save
                       </Button>
+                    )}
                     {user.isAdmin && (
                       <button
                         type="button"
@@ -380,26 +381,6 @@ export default function Ticket() {
                         className="inline-flex justify-center items-center gap-x-1.5 rounded-md bg-white px-5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                       >
                         {data.ticket.hidden ? "Show Global" : "Hide Ticket"}
-                      </button>
-                    )}
-                    {!edit ? (
-                      <button
-                        type="button"
-                        onClick={() => setEdit(true)}
-                        className="inline-flex justify-center items-center gap-x-1.5 rounded-md bg-white px-5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      >
-                        {t("edit-btn")}
-                      </button>
-                    ) : (
-                      <button
-                        type="button"
-                        onClick={() => {
-                          update();
-                          setEdit(false);
-                        }}
-                        className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      >
-                        {t("save")}
                       </button>
                     )}
                   </div>
@@ -459,6 +440,7 @@ export default function Ticket() {
                                     : n
                                     ? n.name
                                     : t("select_new_user")}
+                                </span>
                               </Listbox.Button>
 
                               <Transition
