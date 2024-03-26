9e136827143497702176787634062d268405305d
Sellab <102852787+BobThePogBuilder@users.noreply.github.com>
2024-03-26 01:51:26 -0700

    Updated index.tsx
    Changed copyright year

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -464,7 +464,7 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
           <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
             <p className="text-xs leading-5 text-gray-500">
-              &copy; 2023 Peppermint Labs Ltd. All rights reserved.
+              &copy; 2024 Peppermint Labs Ltd. All rights reserved.
             </p>
           </div>
         </div>
