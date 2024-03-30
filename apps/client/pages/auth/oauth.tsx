8e0f39580e78774fc105260861458db4403b873e
potts99 <japotts16@gmail.com>
2024-03-30 10:43:27 +0000

    fixed sso via github

--- apps/client/pages/auth/oauth.tsx
+++ apps/client/pages/auth/oauth.tsx
@@ -2,64 +2,37 @@ import { setCookie } from "cookies-next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export async function getServerSideProps({ query }: any) {
-  //   console.log(query);
-
-  if (process.env.API_URL.includes("localhost")) {
-    const sso = await fetch(
-      `/api/v1/auth/sso/login/callback?code=${query.code}&state=${query.state}`
-    ).then((res) => res.json());
-
-    if (!sso.success) {
-      return {
-        redirect: {
-          destination: "/auth/login?error=account_not_found",
-          permanent: false,
-        },
-      };
-    } else {
-      return {
-        props: {
-          token: sso.token,
-          permanent: true,
-        },
-      };
-    }
-  } else {
-    const sso = await fetch(
-      `/api/v1/auth/sso/login/check?code=${query.code}&state=${query.state}`
-    ).then((res) => res.json());
-    console.log(sso);
-
-    if (!sso.success) {
-      return {
-        redirect: {
-          destination: "/auth/login?error=account_not_found",
-          permanent: false,
-        },
-      };
-    } else {
-      return {
-        props: {
-          token: sso.token,
-          permanent: true,
-        },
-      };
-    }
-  }
-}
-
-export default function Login({ token }) {
+export default function Login() {
   const router = useRouter();
 
-  function setandRedirect() {
+  console.log(router);
+
+  async function check() {
+    if (router.query.code) {
+      console.log("hit");
+
+      const sso = await fetch(
+        `/api/v1/auth/sso/login/callback?code=${router.query.code}`
+      ).then((res) => res.json());
+
+      console.log("SSO", sso);
+
+      if (!sso.success) {
+        router.push("/auth/login?error=account_not_found");
+      } else {
+        setandRedirect(sso.token);
+      }
+    }
+  }
+
+  function setandRedirect(token) {
     setCookie("session", token, { maxAge: 60 * 6 * 24 });
     router.push("/onboarding");
   }
 
   useEffect(() => {
-    setandRedirect();
-  }, []);
+    check();
+  }, [router]);
 
   return <div></div>;
 }
