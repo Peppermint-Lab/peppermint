4d2fc2b33a02762277441b187382f94c393fa6d2
Jack Andrews <japotts16@gmail.com>
2024-06-10 15:27:15 +0100

    fix: remove profile from list

--- apps/client/layouts/settings.tsx
+++ apps/client/layouts/settings.tsx
@@ -24,33 +24,6 @@ export default function Settings({ children }) {
             <aside className="py-6 lg:col-span-3">
               <nav>
                 <Link
-                  href="/settings/profile"
-                  className={
-                    router.pathname === "/settings/profile"
-                      ? linkStyles.active
-                      : linkStyles.inactive
-                  }
-                  aria-current="page"
-                >
-                  <svg
-                    className="text-teal-500 group-hover:text-teal-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span className="truncate">{t("profile")}</span>
-                </Link>
-
-                <Link
                   href="/settings/notifications"
                   className={
                     router.pathname === "/settings/notifications"
