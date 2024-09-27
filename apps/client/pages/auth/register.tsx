0beaae63485c8809f79da2a4c4740ff19e2e4ced
ExtremeXBB <QQ754163444@gmail.com>
2024-09-27 23:37:25 +0100

    feat(i18n): simplified chinese language added (#361)
    Co-authored-by: Jack Andrews <45759583+potts99@users.noreply.github.com>

--- apps/client/pages/auth/register.tsx
+++ apps/client/pages/auth/register.tsx
@@ -155,6 +155,7 @@ export default function Login({}) {
                     <option value="tr">Turkish</option>
                     <option value="hu">Hungarian</option>
                     <option value="th">Thai (ภาษาไทย)</option>
+                    <option value="zh-CN">Simplified Chinese (简体中文)</option>
                   </select>
                 </div>
               </div>
