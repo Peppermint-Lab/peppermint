7b12f30ef83614ff4367928760c41d36139e27a1
jackandrews <jack.andrews@airtimerewards.com>
2024-10-22 14:03:56 +0100

    fix: avoid drop null

--- apps/api/src/prisma/migrations/20241021230158_redirecturi/migration.sql
+++ apps/api/src/prisma/migrations/20241021230158_redirecturi/migration.sql
@@ -1,9 +1,2 @@
-/*
-  Warnings:
-
-  - Made the column `expiresIn` on table `Email` required. This step will fail if there are existing NULL values in that column.
-
-*/
 -- AlterTable
-ALTER TABLE "Email" ADD COLUMN     "redirectUri" TEXT,
-ALTER COLUMN "expiresIn" SET NOT NULL;
+ALTER TABLE "Email" ADD COLUMN "redirectUri" TEXT;
